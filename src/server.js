e2343fb17b8580b87df0a5ed14f26890ec50537d
Izaque Rodrigues <izaque.jr@users.noreply.github.com>
2020-06-15 20:33:16 -0300

    Correção de erro
    Corrigindo erro de escrita

--- src/server.js
+++ src/server.js
@@ -83,7 +83,7 @@ server.get("/search", (req, res) => {
   }
 
   // Pegar os dados do banco de dados
-  db.all(`SELECT * FROM places WHERE city LIKE = '%${search}%'`, function (err, rows) {
+  db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function (err, rows) {
     if (err) {
       return console.log(err);
     }
